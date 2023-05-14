39ee21be5339d61145ec77344ae86877ae5aafdb
Ryan Shin <ryanminjaeshin@gmail.com>
2023-05-14 13:57:25 +0900

    update

--- src/main/java/jpabook/jpashop/HelloController.java
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -10,6 +10,7 @@ public class HelloController {
     // hello url로 오면 hello controller 호출
     @GetMapping("hello")
     public String hello(Model model) {
-
+        model.addAttribute("data", "hello!!!");
+        return "hello";// returning hello.html on template folder
     }
 }
