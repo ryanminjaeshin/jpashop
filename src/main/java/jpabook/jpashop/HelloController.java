e04dd2f86ce5bad55b588e0d2f36f41e6ff17cf8
Ryan Shin <ryanminjaeshin@gmail.com>
2023-05-14 13:43:08 +0900

    add controller

---
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    // hello url로 오면 hello controller 호출
+    @GetMapping("hello")
+    public String hello(Model model) {
+
+    }
+}
