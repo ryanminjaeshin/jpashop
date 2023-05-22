757610d50e601d1a33f9b41f6274886fc6be7423
Ryan Shin <ryanminjaeshin@gmail.com>
2023-05-22 22:12:54 +0900

    WIP

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -4,6 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 // Member와 Order는 양방향 연결관계
 @Entity
@@ -19,4 +22,12 @@ public class Order {
     @ManyToOne
     @JoinColumn(name="member_id") // FK = member_id
     private Member member;
+
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private Delivery delivery;
+
+    private LocalDateTime orderDate;
+
+    private OrderStatus status; //주문 상태(Order,Cancel)
 }
