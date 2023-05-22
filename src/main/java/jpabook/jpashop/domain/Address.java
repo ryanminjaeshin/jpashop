3e4411d009c471496d55b55fd59d9360e589612f
Ryan Shin <ryanminjaeshin@gmail.com>
2023-05-22 21:49:41 +0900

    Finish address

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,4 +1,16 @@
 package jpabook.jpashop.domain;
 
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+// JPA 내장될 수 있다 (어딘가에)
+@Embeddable
+@Getter
 public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
 }
