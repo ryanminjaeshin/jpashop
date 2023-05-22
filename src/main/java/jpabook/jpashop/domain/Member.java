8c9c81161fa28eb524ded91c71d6ca6cc4ce1cff
Ryan Shin <ryanminjaeshin@gmail.com>
2023-05-22 21:53:42 +0900

    add Embedded annotation

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -3,10 +3,7 @@ package jpabook.jpashop.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +17,7 @@ public class Member {
 
     private String name;
 
+    @Embedded
     private Address address;
 
     private List<Order> orders = new ArrayList<>();
