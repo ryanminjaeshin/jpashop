ce7b167167334a490fd97488ac71e6dabdb151ae
Ryan Shin <ryanminjaeshin@gmail.com>
2023-05-23 00:35:56 +0900

    Add inheritance and discriminator annotations

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -3,13 +3,14 @@ package jpabook.jpashop.domain.item;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+// 상속관계 전략 설정
+// Single table 전략
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
 public abstract class Item {
 
     @Id
