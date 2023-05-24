97b50dd076640fa8eedc0576e674435bc9aa113d
Ryan Shin <ryanminjaeshin@gmail.com>
2023-05-24 23:15:37 +0900

    add JoinColumns anno

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -20,6 +20,9 @@ public class Category {
     private String name;
 
     @ManyToMany
+    @JoinTable(name = "category_item",
+        joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id")) // 중간 테이블 mapping; 다대다 mapping을 일대다 다대일로 풀어냄; but 실무에서 안씀.
     private List<Item> items = new ArrayList<>();
 
 }
