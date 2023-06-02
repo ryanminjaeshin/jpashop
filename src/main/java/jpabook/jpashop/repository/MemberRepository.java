56c7bae6abe84465aabbe85a66ce01a47a12c691
Ryan Shin <ryanminjaeshin@gmail.com>
2023-06-02 22:39:25 +0900

    add EntityManager

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -2,6 +2,13 @@ package jpabook.jpashop.repository;
 
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 @Repository
 public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
 }
